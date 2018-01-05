a84217c88b25092ff23a527dcc428b1fc48db4a0
Francheska <francheska-guzman@users.noreply.github.com>
2018-01-05 17:58:51 -0500

    Updating

--- angular/src/app/list-of-songs/list-of-songs.component.ts
+++ angular/src/app/list-of-songs/list-of-songs.component.ts
@@ -7,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListOfSongsComponent implements OnInit {
   pageTitle: string = 'List of Songs';
-  
+  // In typescript "any" is a data type.
+  songs: any[] = [{
+      "songId": 1,
+      "songName": "La Fiesta de Pilito",
+      "time": "3:55",
+      "artist": "El Gran Combo de Puerto Rico",
+      "album": "Nuestra Música",
+      "genre": "Salsa",
+      "starRating": 4.2,
+      "imageUrl": "https://i.scdn.co/image/404f1635238ba205e075539a174482ef432e4269"
+    },
+    {
+      "songId": 2,
+      "songName": "Olé Maja",
+      "time": "3:19",
+      "artist": "Quique Domenech",
+      "album": "Lo Mejor de Quique Domenech",
+      "genre": "Folk",
+      "starRating": 5.0,
+    }
+  ];
   constructor() { 
   }
 
