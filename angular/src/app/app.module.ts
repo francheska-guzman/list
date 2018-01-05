84458f6566234762af873f1c9bedc9b1852615be
Francheska <francheska-guzman@users.noreply.github.com>
2018-01-05 13:18:17 -0500

    Adding new component.

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SongsListComponent } from './songs-list/songs-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SongsListComponent,
   ],
   imports: [
     BrowserModule
