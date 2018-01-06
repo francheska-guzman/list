81b0ad59e79d670492f0c7d1b2c01d5d62d89d20
Francheska <francheska-guzman@users.noreply.github.com>
2018-01-06 11:39:10 -0500

    Updating

--- angular/src/app/footer/footer.component.ts
+++ angular/src/app/footer/footer.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
+  // This is an inline template. Is an excellent option when is a short code.
+  template: `
+  <p class='footer'>
+  Handmade my <a class='url' target='_blank' href='http://francheskaguzman.io'>Francheska Guzman</a>.
+  </p>`,
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
