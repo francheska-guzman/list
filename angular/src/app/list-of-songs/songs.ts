8c6aa34296a557115141f256008fd820cf70b3f8
Francheska <francheska-guzman@users.noreply.github.com>
2018-01-12 17:34:51 -0500

    Updating

--- angular/src/app/list-of-songs/songs.ts
+++ angular/src/app/list-of-songs/songs.ts
@@ -1,15 +1,15 @@
 // Using the 'I' prefex to distinguish the interface.
 // Defines custom types to avoid syntax error.
 export interface ISongs {
-    'songId': number;
-    'songName': string;
-    'time': string;
-    'artist': string;
-    'album': string;
-    'releaseDate': number;
-    'genre': string;
-    'starRating': number;
-    'imageUrl': string;
-    'songUrl': string;
-    'songLyrics': string;
+    songId: number;
+    songName: string;
+    time: string;
+    artist: string;
+    album: string;
+    releaseDate: number;
+    genre: string;
+    starRating: number;
+    imageUrl: string;
+    songUrl: string;
+    songLyrics: string;
 }
