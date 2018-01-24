3d110068e6769ccb1daabd8fde1b5693d6ad6672
Francheska <francheska-guzman@users.noreply.github.com>
2018-01-24 14:49:36 -0500

    Adding album image.

--- angular/src/app/songs/song-detail.component.ts
+++ angular/src/app/songs/song-detail.component.ts
@@ -1,48 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
-import { ISong } from './song';
-
+import { ActivatedRoute, Router } from '@angular/router';
 // I need to import Router to add a "back" button.
-import { Router } from '@angular/router';
+import { ISong } from './song';
+import { SongService } from './song.service';
 
 @Component({
-  selector: 'song-detail',
   templateUrl: './song-detail.component.html',
   styleUrls: ['../app.component.css', './song-detail.component.css']
 })
 export class SongDetailComponent implements OnInit {
   pageTitle: string = 'Song Detail';
   song: ISong;
+  errorMessage: string;
 
-  constructor(private _route: ActivatedRoute, 
-  			  private _router: Router) { 
-  	// console.log(this._route.snapshot.paramMap.get('id'));
-  }
+  constructor(private _route: ActivatedRoute,
+    private _router: Router,
+    private _songService: SongService) { /* console.log(this._route.snapshot.paramMap.get('id')); */ }
 
   // We use _route
   ngOnInit() {
-  	// The plus + is a JS shortcut to convert the parameter string to numeric.
-  	let id = +this._route.snapshot.paramMap.get('id');
-  	this.song = {
-  		"songId": id,
-        "songName": "La Fiesta de Pilito",
-        "time": "3:55",
-        "artist": "El Gran Combo de Puerto Rico",
-        "album": "Nuestra Música",
-        "releaseDate": 1985,
-        "genre": "Salsa",
-        "starRating": 4.5,
-        "imageUrl": "https://i.scdn.co/image/404f1635238ba205e075539a174482ef432e4269",
-        "songUrl": " ",
-        "songLyrics": " "
-  	}
+  // The plus + is a JS shortcut to convert the parameter string to numeric.
+  const param = +this._route.snapshot.paramMap.get('id');
+    if (param) {
+      const id = +param;
+      this.getSong(id);
+    }
+  }
+
+  getSong(id: number) {
+    this._songService.getSong(id).subscribe (
+      song => this.song = song,
+      error => this.errorMessage = <any>error);
   }
 
   // We use _router
   onBack(): void {
-  	this._router.navigate(['/songs']);
+    this._router.navigate(['/songs']);
   }
-
 
 }
