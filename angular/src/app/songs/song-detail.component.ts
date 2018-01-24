dadebabaa36eda57596dd89e8963748c51ebac65
Francheska <francheska-guzman@users.noreply.github.com>
2018-01-24 13:11:32 -0500

    Adding back button.

--- angular/src/app/songs/song-detail.component.ts
+++ angular/src/app/songs/song-detail.component.ts
@@ -3,6 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 
 import { ISong } from './song';
 
+// I need to import Router to add a "back" button.
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'song-detail',
   templateUrl: './song-detail.component.html',
@@ -12,11 +15,24 @@ export class SongDetailComponent implements OnInit {
   pageTitle: string = 'Song Detail';
   song: ISong;
 
-  constructor(private _route: ActivatedRoute) { 
-  	console.log(this._route.snapshot.paramMap.get('id'));
+  constructor(private _route: ActivatedRoute, 
+  			  private _router: Router) { 
+  	// console.log(this._route.snapshot.paramMap.get('id'));
   }
 
+  // We use _route
   ngOnInit() {
+  	// The plus + is a JS shortcut to convert the parameter string to numeric.
+  	let id = +this._route.snapshot.paramMap.get('id');
+  	this.song = {
+  		"songName": "Test";
+  	}
   }
+
+  // We use _router
+  onBack(): void {
+  	this._router.navigate(['/songs']);
+  }
+
 
 }
