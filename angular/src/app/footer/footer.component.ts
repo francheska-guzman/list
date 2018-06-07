936be5daa85c787658d1741cf7f252071558d430
Francheska <francheska-guzman@users.noreply.github.com>
2018-06-07 09:45:12 -0400

    Updating URL

--- angular/src/app/footer/footer.component.ts
+++ angular/src/app/footer/footer.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   // This is an inline template. Is an excellent option when is a short code.
   template: `
   <p class='footer'>
-  Handmade my <a class='url' target='_blank' href='http://francheskaguzman.io'>Francheska Guzman</a>.
+  Handmade my <a class='url' target='_blank' href='https://github.com/francheska-guzman'>Francheska Guzman</a>.
   </p>`,
   styleUrls: ['../app.component.css', './footer.component.css']
 })
